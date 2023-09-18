b403bfc40ec06b76391a62f1093d6dbb01f1f67d
Nina Schmid <nina.schmid@uni-bonn.de>
2023-09-18 17:35:57 +0200

    first commit

---
+++ analysis/basic_plots_systems.jl
@@ -0,0 +1,44 @@
+using CSV, DataFrames, Plots
+
+base_path = joinpath(pwd(), "inside_demonstrator")
+experiment_name = "Euler_test"
+
+tandler_output_path = joinpath(base_path, "tandler_output/preprocessed/$experiment_name")
+output_path = joinpath(base_path, "analysis/plots/++systems/$experiment_name")
+
+#####################
+# preparation
+#####################
+# preparation
+df = CSV.read(joinpath(tandler_output_path, "concentration_curves.csv"), DataFrame, delim=",")
+# check whether output path exists
+isdir(output_path) || mkdir(output_path)
+
+#####################
+# Create plots
+#####################
+
+# each location individually
+for location in unique(df.location)
+    df_sub = df[(df.location.==location),:]
+    t = df_sub.time 
+    c = df_sub[!,"COV19"] 
+    p1 = plot(t/60, c, xlabel="t (h)", label=missing, ylabel="RNA concentration", thickness_scaling = 1.6)
+    savefig(p1, joinpath(output_path, "concentration_$location.png"))
+end
+
+
+# all of them together
+location = unique(df.location)[1]
+df_sub = df[(df.location.==location),:]
+t = df_sub.time 
+c = df_sub[!,"COV19"] 
+p1 = plot(t/60, c, xlabel="t (h)", label=location, ylabel="RNA concentration", thickness_scaling = 1.2)
+for location in unique(df.location)[2:end]
+    df_sub = df[(df.location.==location),:]
+    t = df_sub.time 
+    c = df_sub[!,"COV19"] 
+    plot!(t/60, c, xlabel="t (h)", label=location, ylabel="RNA concentration", thickness_scaling = 1.2, legend=:outertopright)
+end
+p1
+savefig(p1, joinpath(output_path, "concentration_all_stations.png"))
