0a28747b5c461c76ef4766e63bf816caecf0651d
Nina Schmid <nina.schmid@uni-bonn.de>
2023-09-19 14:31:47 +0200

    run a first experiment on the different rain scenarios

--- analysis/basic_plots_systems.jl
+++ analysis/basic_plots_systems.jl
@@ -1,7 +1,7 @@
 using CSV, DataFrames, Plots
 
 base_path = joinpath(pwd(), "inside_demonstrator")
-experiment_name = "Euler_test"
+experiment_name = "last_day/MittelstarkerRegen"
 
 tandler_output_path = joinpath(base_path, "tandler_output/preprocessed/$experiment_name")
 output_path = joinpath(base_path, "analysis/plots/++systems/$experiment_name")
@@ -12,7 +12,8 @@ output_path = joinpath(base_path, "analysis/plots/++systems/$experiment_name")
 # preparation
 df = CSV.read(joinpath(tandler_output_path, "concentration_curves.csv"), DataFrame, delim=",")
 # check whether output path exists
-isdir(output_path) || mkdir(output_path)
+
+isdir(output_path) || mkpath(output_path)
 
 #####################
 # Create plots
