ed8c61e7bbef81bc8ac4d632b46a988cb2740bf1
Nina Schmid <nina.schmid@uni-bonn.de>
2024-01-09 15:15:24 +0100

    make compare flow rates more flexible

--- analysis/systems/compare_flow_rates.jl
+++ analysis/systems/compare_flow_rates.jl
@@ -1,18 +1,20 @@
 using CSV, DataFrames, Plots
 
-base_path = joinpath(pwd(), "inside_demonstrator")
-experiment_name = "2_weeks"
+base_path = joinpath(pwd())
+experiment_name = "final_tests"
+hydraulic_setting = "b_constant_hydraulics_same"
 
 tandler_output_path = joinpath(base_path, "tandler_output/preprocessed/$experiment_name")
 output_path = joinpath(base_path, "analysis/plots/++systems/$experiment_name")
 
 # load data
-df_Nieselregen = CSV.read(joinpath(tandler_output_path, "Nieselregen/flow_rates.csv"), DataFrame, delim=",")
-df_MittelstarkerRegen = CSV.read(joinpath(tandler_output_path, "MittelstarkerRegen/flow_rates.csv"), DataFrame, delim=",")
-df_keinRegen = CSV.read(joinpath(tandler_output_path, "keinRegen/flow_rates.csv"), DataFrame, delim=",")
+df_keinRegen = CSV.read(joinpath(tandler_output_path, "1_KeinRegen/$(hydraulic_setting)/flow_rates.csv"), DataFrame, delim=",")
+df_Nieselregen = CSV.read(joinpath(tandler_output_path, "2_Nieselregen/$(hydraulic_setting)/flow_rates.csv"), DataFrame, delim=",")
+df_MittelstarkerRegen = CSV.read(joinpath(tandler_output_path, "3_MittelstarkerRegen/$(hydraulic_setting)/flow_rates.csv"), DataFrame, delim=",")
 
 # plot for one location
 loc = "RW157"
+
 df_sub = dropmissing(df_Nieselregen, :location)
 df_sub = df_sub[df_sub.location.==loc,:]
 plot(df_sub.time, df_sub.flow_rate, label="Nieselregen", xlabel="time (min)", ylabel="flow rate (l/s)", title="$loc")
@@ -22,3 +24,12 @@ plot!(df_sub.time, df_sub.flow_rate, label="MittelstarkerRegen")
 df_sub = dropmissing(df_keinRegen, :location)
 df_sub = df_sub[df_sub.location.==loc,:]
 plot!(df_sub.time, df_sub.flow_rate, label="keinRegen", xlabel="time (min)", ylabel="flow rate (l/s)")
+savefig(joinpath(output_path, "flow_rate_$loc.png"))
+
+df_sub = dropmissing(df_Nieselregen, :location)
+df_sub = df_sub[df_sub.location.==loc,:]
+plot(df_sub.time, df_sub.flow_rate, label="Nieselregen", xlabel="time (min)", ylabel="flow rate (l/s)", title="$loc")
+df_sub = dropmissing(df_keinRegen, :location)
+df_sub = df_sub[df_sub.location.==loc,:]
+plot!(df_sub.time, df_sub.flow_rate, label="keinRegen", xlabel="time (min)", ylabel="flow rate (l/s)")
+savefig(joinpath(output_path, "flow_rate_kein_und_niesel_$loc.png"))
