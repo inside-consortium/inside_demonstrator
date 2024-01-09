9cd6fe449ecdf59d4c2ef205f78d113c48c7c426
Nina Schmid <nina.schmid@uni-bonn.de>
2024-01-09 15:04:29 +0100

    more detailed concentration analysis

--- analysis/systems/compare_concentrations_for_one_experiment.jl
+++ analysis/systems/compare_concentrations_for_one_experiment.jl
@@ -1,7 +1,13 @@
 using CSV, DataFrames, Plots
 
-base_path = joinpath(pwd(), "inside_demonstrator")
-experiment_name = "2_weeks/MittelstarkerRegen"
+base_path = joinpath(pwd())
+experiment_name = "final_tests/3_MittelstarkerRegen/b_constant_hydraulics_same"
+"""
+tandler_output/preprocessed/final_tests/1_KeinRegen/0_recalculate_hydraulics/concentration_curves.csv
+final_tests/1_KeinRegen/0_recalculate_hydraulics
+final_tests/2_Nieselregen/a_constant_hydraulics_avg
+final_tests/3_MittelstarkerRegen/b_constant_hydraulics_same
+"""
 
 tandler_output_path = joinpath(base_path, "tandler_output/preprocessed/$experiment_name")
 output_path = joinpath(base_path, "analysis/plots/++systems/$experiment_name")
@@ -27,7 +33,6 @@ for location in unique(df.location)
     p1 = plot(t/60, c, xlabel="t (h)", label=missing, ylabel="RNA concentration", thickness_scaling = 1.6)
     savefig(p1, joinpath(output_path, "concentration_$location.png"))
 end
-
 
 # all of them together
 location = unique(df.location)[1]
